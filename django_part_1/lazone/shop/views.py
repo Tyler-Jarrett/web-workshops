ae6e22d5c05b9f676572c255f19fc001d2c6bf29
Tyler Schwehr <tylerschwehr04@gmail.com>
2025-01-10 01:36:15 -0500

    Did some more work on the solution for today's workshop

--- django_part_1/lazone/shop/views.py
+++ django_part_1/lazone/shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def index(request):
     return render(request, 'shop/home.html')
 
 def list_view(request):
-    return render(request, 'home.html')
+    product_list = Item.objects.all()
+
+    return render(request, 'shop/products.html', {'product_list': product_list})
