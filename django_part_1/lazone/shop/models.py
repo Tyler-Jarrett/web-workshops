77bd04103f499c90b78abcc614c13d0a56aabbc3
Tyler <tylerschwehr04@gmail.com>
2025-01-10 11:51:36 +0800

    Started the demo project, added the files for the workshop

---
+++ django_part_1/lazone/shop/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    itemName = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    cost = models.FloatField()
+    stockCount = models.IntegerField()
